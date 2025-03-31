a3bb45cf242c1dba522a6ebf839c595df8de1f0e
Юра Алекса <aleksayo@MacBook-Pro-Ura.local>
2025-03-31 19:40:28 +0300

    start

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
-const router = require("./routes/api/");
+const axios = require("axios");
+
+const wallets = require("./wallets.json").wallets;
+
+const ETHERSCAN_API = process.env.ETHERSCAN_API;
+
+async function getTransactions(wallet) {
+  const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${wallet}&apikey=${ETHERSCAN_API}`;
+
+  const response = await axios.get(url);
+
+  return response.data.result;
+}
+
+async function monitorWallets() {
+  let a;
+  for (const wallet of wallets) {
+    const transactions = await getTransactions(wallet);
+
+    for (const tx of transactions) {
+      a = analyzeTransaction(tx);
+      console.log(a);
+    }
+  }
+}
+
+monitorWallets();
+
+async function analyzeTransaction(tx) {
+  if (
+    tx.to.toLowerCase() ===
+    "0xE592427A0AEce92De3Edee1F18E0157C05861564".toLowerCase()
+  ) {
+    console.log(`Кит купил токен через Uniswap: ${tx.hash}`);
+
+    return tx;
+  }
+
+  return 123;
+}
+
+// setInterval(monitorWallets, 3000);
+
+// const router = require("./routes/api/");
 
 const app = express();
 
@@ -12,7 +56,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/", router);
+// app.use("/api/", router);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
